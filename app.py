5d54c695b17cc618055026fafb8e8bf763308b82
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-06 20:24:52 -0300

    add sliders

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.write("# Classificação de IRIS")
 st.write('## Exemplo com comprimentos de pétala e sépala')
 
+st.sidebar.write('### Parâmetros')
+st.sidebar.slider('Comprimento da sépala', 4, 8, 5.8, 0.1)
+
+st.sidebar.slider('Comprimento da pétala', 0.9, 7, 3.8, 0.1)
