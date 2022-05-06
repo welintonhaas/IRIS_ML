f50d626c962a75b8f9ac8723844dc556382121e1
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-06 20:59:04 -0300

    imports

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import pickle
 import streamlit as st
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 st.write("# Classificação de IRIS")
 st.write('## Exemplo com comprimentos de pétala e sépala')
@@ -7,3 +10,6 @@ st.sidebar.write('### Parâmetros')
 st.sidebar.slider('Comprimento da sépala', 4.0, 8.0, 5.8, 0.1)
 
 st.sidebar.slider('Comprimento da pétala', 0.9, 7.0, 3.8, 0.1)
+
+with open("objetos.pkl", 'rb') as arquivo:
+  ss, dtc = pickle.load(arquivo)
