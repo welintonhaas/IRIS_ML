1771addae770da074b8bed4de6178125fa98bd25
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-06 20:25:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+%%writefile app.py
 import streamlit as st
 
 st.write("# Classificação de IRIS")
 st.write('## Exemplo com comprimentos de pétala e sépala')
 
 st.sidebar.write('### Parâmetros')
-st.sidebar.slider('Comprimento da sépala', 4, 8, 5.8, 0.1)
+st.sidebar.slider('Comprimento da sépala', 4.0, 8.0, 5.8, 0.1)
 
-st.sidebar.slider('Comprimento da pétala', 0.9, 7, 3.8, 0.1)
+st.sidebar.slider('Comprimento da pétala', 0.9, 7.0, 3.8, 0.1)
