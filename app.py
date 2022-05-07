5a02c278e402b42a611b1f5f9eb7e194bc5d0010
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-06 21:16:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ with open("objetos.pkl", 'rb') as arquivo:
 estrutura = { 'comp_sepala' : comp_sepala, 
               'comp_petala' : comp_petala }
 
-df = pd.DataFrame(estrutura, index[0])
+df = pd.DataFrame(estrutura, index=[0])
 st.write(df)
