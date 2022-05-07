641e4dfa742f4b21013b36e4933bec7ee5a331a5
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-06 21:46:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,13 @@ predicao = dtc.predict(df)
 st.write(f'A classe dessa flor é: **{predicao[0]}**')
 
 predicao = dtc.predict_proba(df)
-predicao.rename( 
-    { 0 : ' Iris-setosa',
-      1 : 'Iris-Versicolor',
-      2 : 'Iris-Virginica' }, axis=1, inplace=True)
+
+predicao = pd.DataFrame(predicao)
+
+predicao.rename({
+     0: 'Iris-setosa',
+     1: 'Iris-Versicolor',
+     2: 'Iris-Virginica' }, axis=1, inplace=True)
 
 st.write('Probabilidades')
 st.write(predicao)
